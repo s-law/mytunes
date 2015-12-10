684edf9002644f666996c9750f43db5493a303b4
s-law <slaw@nuclearanvil.com>
2015-12-10 15:44:27 -0800

    Create a view to display song currently playing. Properly handles case where initially no song is loaded.

---
+++ client/views/CurrentSongView.js
@@ -0,0 +1,25 @@
+var CurrentSongView = Backbone.View.extend({
+
+	tagName: 'span',
+
+	template: _.template('<span class="label">Current Song: </span><span class="artist">(<%= artist %>)&nbsp;</span><span class="title"><%= title %></span>'),
+
+	initialize: function() {
+		console.log(this.model);
+		this.render();
+	},
+
+	//click event to stop playing,
+	//load next song?
+
+	setSong: function(song) {
+    this.model = song;
+    this.render();
+  },
+
+	render: function() {
+		var holder = this.model.get('title') ? this.template(this.model.attributes) : '<span class="label">Current Song: </span><span>[no song loaded]</span>';
+		return this.$el.html(holder);
+	}
+
+});
