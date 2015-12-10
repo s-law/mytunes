72c203610271f14230bc4d24e302dd25e3354812
s-law <slaw@nuclearanvil.com>
2015-12-09 20:35:45 -0800

    Commented out unnecessary initialization

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,6 +1,13 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
   // your code here!
+
+  // initialize: function() {
+  // 	this.model.on('change', function() {
+  // 		this.render();
+  // 	}, this);
+  // },
+
   tagName: 'tr',
 
   template: _.template('<td>(<%= artist %>)</td><td><%= title %></td>'),
