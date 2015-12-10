995eecc731435aa7125b8959723b669ba38a8ca3
s-law <slaw@nuclearanvil.com>
2015-12-10 12:34:49 -0800

    New playcount property and related functionality

--- client/models/SongModel.js
+++ client/models/SongModel.js
@@ -1,12 +1,19 @@
 // SongModel.js - Defines a backbone model class for songs.
 var SongModel = Backbone.Model.extend({
 
+  defaults: {
+    playCount: 0
+  },
+
   play: function() {
     // Triggering an event here will also trigger the event on the collection
+
     this.trigger('play', this);
   },
 
   ended: function() {
+    this.set('playCount', this.get('playCount') + 1);
+    console.log(this);
   	this.trigger('ended', this);
   },
 
