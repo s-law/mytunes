7db48be2ac6fae2f90e56ce5ea54913285727d9d
s-law <slaw@nuclearanvil.com>
2015-12-10 15:42:09 -0800

    Added connections to CurrentSongView

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -3,6 +3,7 @@ var AppView = Backbone.View.extend({
 
   initialize: function(params) {
     this.playerView = new PlayerView({model: this.model.get('currentSong')});
+    this.currentSongView = new CurrentSongView({model: this.model.get('currentSong')});
     this.libraryView = new LibraryView({collection: this.model.get('library')});
     this.songQueueView = new SongQueueView({collection: this.model.get('songQueue')});
 
@@ -10,13 +11,14 @@ var AppView = Backbone.View.extend({
     // ONLY receive change events for the specific property, 'currentSong'
     this.model.on('change:currentSong', function(model) {
       this.playerView.setSong(model.get('currentSong'));
+      this.currentSongView.setSong(model.get('currentSong'));
     }, this);
   },
 
   render: function() {
     // maybe add a class to this?
     var $wrapperPlayer = $('<div class="player"></div>')
-    $wrapperPlayer.html([this.playerView.$el]);
+    $wrapperPlayer.html([this.currentSongView.$el, this.playerView.$el]);
     var $wrapperQueue = $('<div></div>');
     $wrapperQueue.html([this.libraryView.$el, this.songQueueView.$el]);
     return this.$el.html([
