41993f29e5523c17e1bb942436d01472dbc5de4a
s-law <slaw@nuclearanvil.com>
2015-12-09 20:55:46 -0800

    Replaced non-working media.

--- client/data/data.js
+++ client/data/data.js
@@ -6,36 +6,23 @@ var songData = [
     artist: "Ainst",
   },
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
+    url: "http://soundbible.com/mp3/Halloween_Vocals-Mike_Koenig-517765553.mp3",
     title: "One In A Million",
     artist: "Aaliyah",
   },
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/03+Age+Ain%27t+Nothing+But+A+Number.mp3",
+    url: "http://soundbible.com/mp3/Zymbel_The_Real_Horst-1113884951.mp3",
     title: "Age Ain't Nothing But A Number",
     artist: "Aaliyah",
   },
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/05+Hot+Like+Fire.mp3",
+    url: "http://soundbible.com/mp3/Toke_and_Exhale-Jaime_Os_Rivera-930234911.mp3",
     title: "Hot Like Fire",
     artist: "Aaliyah",
   },
   {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/06+If+Your+Girl+Only+Knew.mp3",
+    url: "http://soundbible.com/mp3/fighter_plane_squadron-Mike_Koenig-1917709101.mp3",
     title: "If Your Girl Only Knew",
     artist: "Aaliyah",
   }
 ];
-
-var fakeData = [
-{
-    url: "https://ia600807.us.archive.org/30/items/Ainst-Spaceshipdemo.mp3/Ainst-Spaceshipdemo.mp3",
-    title: "Stuff",
-    artist: "Faux Fighters",
-  },
-  {
-    url: "https://s3-us-west-1.amazonaws.com/hr-mytunes/data/04+One+In+A+Million.mp3",
-    title: "More Stuff",
-    artist: "Bob",
-  }
-]
